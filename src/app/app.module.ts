05ca4bf5eb3fba832888aac744e2d42b0d8119ab
SKY <onthsky@gmail.com>
2022-10-30 00:00:00 +0300

    New Pipe, component and module has added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { CategoryComponent } from './components/category/category.component';
 import{HttpClientModule} from '@angular/common/http';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe'
-import {ToastrModule} from 'ngx-toastr'
+import {ToastrModule} from 'ngx-toastr';
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component'
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import {ToastrModule} from 'ngx-toastr'
     NaviComponent,
     CategoryComponent,
     VatAddedPipe,
-    FilterPipePipe
+    FilterPipePipe,
+    CartSummaryComponent
   ],
   imports: [
     BrowserModule,
